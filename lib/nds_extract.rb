0531770d67ad254eb3d8cd33aab0c17d7cc28ba1
calm-fork-7535 <komald.4@gmail.com>
2020-01-22 21:15:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,7 +7,19 @@ def directors_totals(nds)
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-
+  ret_hash= {}
+  index = 0
+  while nds[index] do
+    sum = 0
+    num_movies=0
+    while nds[index][:movies][num_movies] do
+      sum += nds[index][:movies][num_movies][:worldwide_gross]
+      num_movies += 1
+    end
+    ret_hash[nds[index][:name]]= sum
+    index += 1
+  end
+  
   result = {
 }
   #
